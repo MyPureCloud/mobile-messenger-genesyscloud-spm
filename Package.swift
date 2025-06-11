504c3634a8d02047662fcb05e78811fcd6298272
Andras Solyom <andras.solyom@genesys.com>
2025-06-11 08:59:53 +0200

    [Update] 1.14.0

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
         .package(
             name: "GenesysCloudMessenger",
             url: "https://github.com/MyPureCloud/mobile-messenger-genesyscloudmessenger-spm.git",
-            .exact("1.13.0")
+            .exact("1.14.0")
         )
     ],
     targets: [
         .binaryTarget(
             name: "GenesysCloud",
-            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.prod/GenesysCloud/GenesysCloud_version_t1.13.0_commit_45de8602fe8c4ae02beb2dc8c8dd5618a89feca2.zip",
-            checksum: "d57ba9cb4e16f245094cdb7c6e140a656e6665d28b3454cd37e09af99ce12db9"
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.prod/GenesysCloud/GenesysCloud_version_t1.14.0_commit_b7d51b8d8c06fd12512c1f9c395eb36f91a59d5f.zip",
+            checksum: "13611833a1dd6317c106fd2ca434d8efe0c7920b0bd996a0649090331462cd73"
         ),
         .target(
             name: "GenesysCloudWrapper",
